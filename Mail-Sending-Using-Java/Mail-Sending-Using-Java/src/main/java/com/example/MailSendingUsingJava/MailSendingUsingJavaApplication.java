68a23b15b5b6d75133d50b0511008c01bf371592
Mukesh Lomror <54076057+mukeshlomror@users.noreply.github.com>
2023-09-11 15:05:32 +0530

    Uploading files to github

---
+++ Mail-Sending-Using-Java/Mail-Sending-Using-Java/src/main/java/com/example/MailSendingUsingJava/MailSendingUsingJavaApplication.java
@@ -0,0 +1,14 @@
+package com.example.MailSendingUsingJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailSendingUsingJavaApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MailSendingUsingJavaApplication.class, args);
+	}
+
+}
---
+++ Mail-Sending-Using-Java/Mail-Sending-Using-Java/src/test/java/com/example/MailSendingUsingJava/MailSendingUsingJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MailSendingUsingJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailSendingUsingJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
